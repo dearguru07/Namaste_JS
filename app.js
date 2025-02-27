5345a3048220bda1d0afc7f5ed84e07fa68ab84b
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-27 20:32:28 +0530

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
