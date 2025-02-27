4ef5089c77618ea1e3010991f095a3209bd7492e
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-27 20:34:52 +0530

    jss

--- hello.js
+++ hello.js
@@ -164,3 +164,103 @@ var squr2=squr(n)
 var squr3=squr(5)
 console.log(squr2)
 console.log(squr3);
+
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
+// console.log('Hello World......');
+
+var n=2
+function squr(num){
+    var ans=num*num
+    return ans
+}
+var squr2=squr(n)
+var squr3=squr(5)
+console.log(squr2)
+console.log(squr3);
