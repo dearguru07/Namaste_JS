67a45c9c83f5dce4740a30d27ff6f68014bd1c15
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-28 12:16:57 +0530

    g

--- app.js
+++ app.js
@@ -10,18 +10,45 @@
 // console.log(squr2)
 // console.log(squr3);
 
-var a = 20;
-function b() {
-  console.log("Hello world");
-}
-b();
+// var a = 20;
+// function b() {
+//   console.log("Hello world");
+// }
+// b();
 
-// let c=25
+// let v=25
 
-var v;
-if(v===undefined){
-    console.log('v is undefinerd');   
-}
-else{
-    console.log('v is not undefined');
-}
+// let g=20
+// if(v===undefined){
+//     console.log('v is undefinerd');
+// }
+// else{
+//     console.log('v is not undefined');
+// }
+
+const radius = [2, 5, 8, 4, 3];
+
+// const area = function (radius) {
+//   return ;
+// };
+
+const Calculate = function (radius) {
+  const output = [];
+for (let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+console.log(Calculate(radius));
+
+
+// let radius = [3, 4, 8, 5];
+
+// function CalculateAre(radius) {
+//   const output = [];
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.PI * radius[i] * radius[i]);
+//   }
+//   return output;
+// }
+// console.log(CalculateAre(radius));
