935a5f9ed0a37c41d98368a386411cdd7129c46e
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-28 10:52:52 +0530

    f

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // console.log('Hello World......');
 
-var n=2
-function squr(num){
-    var ans=num*num
-    return ans
+// var n=2
+// function squr(num){
+//     var ans=num*num
+//     return ans
+// }
+// var squr2=squr(n)
+// var squr3=squr(5)
+// console.log(squr2)
+// console.log(squr3);
+
+var a = 20;
+function b() {
+  console.log("Hello world");
 }
-var squr2=squr(n)
-var squr3=squr(5)
-console.log(squr2)
-console.log(squr3);
+b();
+
+// let c=25
+
+var v;
+if(v===undefined){
+    console.log('v is undefinerd');   
+}
+else{
+    console.log('v is not undefined');
+}
